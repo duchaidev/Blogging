5f1f45ef2595b5b55b272f63a167d106d4ea9480
Hai Dev <duchaidev@gmail.com>
2023-03-16 14:16:47 +0700

    update map

---
+++ src/components/icon/Html5.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Html5 = () => {
+  return (
+    <div>
+      <svg
+        width="40"
+        height="40"
+        viewBox="0 0 40 40"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M20 29.2667L26.7833 27.3833L27.7 17.2167H15.6333L15.3333 13.8333H28L28.3333 10.5167H11.6667L12.6 20.5333H24.0833L23.7 24.8333L20 25.8333L16.3 24.8333L16.0667 22.0667H12.7333L13.2167 27.3833L20 29.2667ZM6.78333 5H33.2167L30.8333 32L20 35L9.16666 32L6.78333 5Z"
+          fill="black"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default Html5;
--- src/modules/map/MapItem.js
+++ src/modules/map/MapItem.js
@@ -1,10 +1,34 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 import Button from "../../components/form/Button";
+import ClipBoard from "../../components/icon/ClipBoard";
+import Html5 from "../../components/icon/Html5";
+import Javas from "../../components/icon/Javas";
+import Reactjs from "../../components/icon/Reactjs";
+import Responsive from "../../components/icon/Responsive";
 
 const MapItem = ({ image, to, title, content }) => {
+  const StyleMapItem = styled.div`
+    .listitem {
+      margin-bottom: 40px;
+      div {
+        width: 50%;
+        path {
+          transition: 0.25s all;
+          fill: gray;
+        }
+        :hover {
+          /* width: 100%; */
+          path {
+            fill: #66fcf1;
+          }
+        }
+      }
+    }
+  `;
   return (
-    <div className=" bg-[#1C2735] p-6 rounded border border-[#495678] flex flex-col gap-3 relative max-w-[35%]">
+    <StyleMapItem className=" bg-[#1C2735] p-6 rounded border border-[#495678] flex flex-col gap-3 relative max-w-[35%]">
       <div className="flex">
         <div className="max-w-[70%]">
           <NavLink to={to} className="text-xl font-bold text-gray-200">
@@ -20,13 +44,31 @@ const MapItem = ({ image, to, title, content }) => {
           />
         </div>
       </div>
-      <div className="mb-8"></div>
-      <div>
-        <Button className="absolute bottom-8 left-5" to={to}>
-          Xem chi tiết
-        </Button>
+      <div className="mb-8">
+        <div className="flex flex-row items-end gap-3 align-bottom listitem">
+          <NavLink className="flex items-center justify-center border rounded-full hover:border-[#66FCF1]">
+            <ClipBoard></ClipBoard>
+          </NavLink>
+          <NavLink className="flex items-center justify-center border rounded-full hover:border-[#66FCF1]">
+            <Html5></Html5>
+          </NavLink>
+          <NavLink className="flex items-center justify-center border rounded-full hover:border-[#66FCF1]">
+            <Responsive></Responsive>
+          </NavLink>
+          <NavLink className="flex items-center justify-center border rounded-full hover:border-[#66FCF1]">
+            <Javas></Javas>
+          </NavLink>
+          <NavLink className="flex items-center justify-center border rounded-full hover:border-[#66FCF1]">
+            <Reactjs></Reactjs>
+          </NavLink>
+        </div>
+        <div>
+          <Button className="absolute bottom-8 left-5" to={to}>
+            Xem chi tiết
+          </Button>
+        </div>
       </div>
-    </div>
+    </StyleMapItem>
   );
 };
 
